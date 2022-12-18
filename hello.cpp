a13f24065968146e66f7310c6a7b5948424ba5d8
liujinchang <2250073040@qq.com>
2022-12-18 13:16:50 +0800

    modify hello c++ file

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout<<"hello wrold"<<endl;
+    cout<<"hello world"<<endl;
+    cout<<"liujinchang learn git how to use"<<endl;
     return 0;
 }
